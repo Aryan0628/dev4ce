ff43d852c0b389115da8db3a03b8824ecb3a4c60
Aryangupta <sandhyagupta5678@gmail.com>
2025-12-05 23:52:25 +0530

    added user model

---
+++ server/src/models/User.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  googleTokens: {
+    type: Object,  // stores access_token, refresh_token, expiry_date, etc.
+    default: null,
+  },
+  lastCalendarSync: { 
+    type: Date, 
+    default: null 
+  },
+  calendarSyncEnabled: { 
+    type: Boolean, 
+    default: false 
+  }
+
+}, { timestamps: true });
+
+export default mongoose.model("User", userSchema);
